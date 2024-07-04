b2836c8b7f2d5200c7c1567ae64cc73acb1a6cd8
Mohamad <mmortada685@gmail.com>
2024-07-03 21:12:33 -0700

    almost done with sign in screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,29 @@
-import { Image, StyleSheet, Platform,  Text, View } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 import React from 'react';
+import { Image, StatusBar, StyleSheet, View, Text } from 'react-native';
+import { Button, Provider as PaperProvider, DefaultTheme } from 'react-native-paper';
+import { SafeAreaFrameContext, SafeAreaView } from 'react-native-safe-area-context';
+
 
 export default function HomeScreen() {
-    return (
-        <View className="flex-1 items-center justify-center">
-            <Text className='text-white'>Home Screen</Text>
+  return (
+    <PaperProvider>
+        <SafeAreaView></SafeAreaView>
+        <View className='container flex justify-center items-center '>
+            <View className='flex justify-evenly items-center'>
+                <Image
+                    className='m-5'
+                    source={require('../assets/images/logo.png')}
+                />
+                <Text className='text-white text-4xl mt-5'>Fremont High School</Text>
+                <Text className='text-white text-2xl mt-2'>Home of the firebirds</Text>
+            </View>
+            <View style={{flexDirection:'row'}}>
+                <Image source={require('../assets/images/schoology.png')}/>
+                <Text>Sign in with Schoology</Text>
+            </View>
         </View>
+    </PaperProvider>
+
     );
 }
+ 
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -17,7 +17,7 @@ export const Colors = {
   },
   dark: {
     text: '#ECEDEE',
-    background: '#151718',
+    background: '#28282B',
     tint: tintColorDark,
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
