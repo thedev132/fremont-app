0e25e906c648188f1bafa8a89ed611361a579a45
Mohamad <mmortada685@gmail.com>
2024-07-25 20:42:39 -0700

    worked on adding clubs

--- app/misc/AddClub.tsx
+++ app/misc/AddClub.tsx
@@ -18,24 +18,53 @@ export default function AddClubScreen({ navigation }) {
     const [noMoreData, setNoMoreData] = useState(false);
     const [allOrgs, setAllOrgs] = useState<Organization[]>([]); // Store all organizations here
 
+    async function addClub(id: number, name: string, type: string) {
+        // Add club to user's clubs
+        // Update myClubs state
+        // Update clubs state
+        let accessToken = await AsyncStorage.getItem('accessToken');
+        const response = await fetch(`https://fremont-app-backend.vercel.app/api/users/me/orgs/`, {
+             "method": "POST",
+             headers: {
+                'Content-Type': 'application/json',
+                "Authorization": `Bearer ${accessToken}`, // Add this line for authorization
+            },
+            "body": `{\n    \"organization\": ${id}\n}`,
+        });
+        let data = await response.json();
+        if (data["organization"] != null) {
+            let newClub = new NestedOrganization(String(id), name, type);
+            setMyClubs((prevClubs) => [...prevClubs, newClub]);
+            setClubs((prevClubs) => prevClubs.filter(club => club.getId() !== String(id)));
+        }
+    }
+
     useEffect(() => {
         const fetchData = async () => {
             try {
-                let me = await getUserMe();
-                let myOrgs = me.getOrgs();
-                
-                setMyClubs(myOrgs);
-
                 setLoading(true);
+                let storedMyOrgs = await AsyncStorage.getItem('myOrgs');
+                if (storedMyOrgs != null) {
+                    let json = JSON.parse(storedMyOrgs);
+                    setMyClubs(json.map((org: any) => new NestedOrganization(org.id, org.name, org.type)));
+                }
+                else {
+                  let me = await getUserMe();
+                  let myOrgs = me.getOrgs();
+                  await AsyncStorage.setItem('myOrgs', JSON.stringify(myOrgs));
+                  setMyClubs(myOrgs);
+                }
+
                 let storedOtherOrgs = await AsyncStorage.getItem('orgs');
+                
                 let orgs: Organization[] = [];
                 if (storedOtherOrgs != null) {
                     let json = JSON.parse(storedOtherOrgs);
                     orgs = json.map((org: any) => new Organization(org.id, org.name, org.type, org.day, org.location, org.time, org.description));
                 } else {
                     orgs = await getAllOrganizations(1);
+                    await AsyncStorage.setItem('orgs', JSON.stringify(orgs));
                 }
-
                 setAllOrgs(orgs);
                 setLoading(false);
             } catch (error) {
@@ -43,7 +72,23 @@ export default function AddClubScreen({ navigation }) {
                 setLoading(false);
             }
         };
+
+        const updateData = async () => {
+            try {
+                let me = await getUserMe();
+                let myOrgs = me.getOrgs();
+                await AsyncStorage.setItem('myOrgs', JSON.stringify(myOrgs));
+                setMyClubs(myOrgs);
+                let orgs: Organization[] = [];
+                orgs = await getAllOrganizations(1);
+                await AsyncStorage.setItem('orgs', JSON.stringify(orgs));
+                setAllOrgs(orgs);
+            } catch (error) {
+                console.error('Error updating organizations:', error);
+            }
+        }
         fetchData();
+        updateData();
     }, []);
 
     useEffect(() => {
@@ -98,9 +143,9 @@ export default function AddClubScreen({ navigation }) {
                     time: item.getTime(),
                     location: item.getLocation()
                 })}>
-                    <View style={{ flexDirection: 'row', alignItems: 'center', flexWrap: 'wrap' }}>
-                        <Text style={{ fontSize: 16, fontWeight: 'bold', flexShrink: 1 }} numberOfLines={2}>{item.getName()}</Text>
-                        <TouchableOpacity style={{ marginLeft: 'auto', marginRight: 5 }}>
+                    <View style={{ flexDirection: 'row', alignItems: 'center', flexWrap: 'NOwrap' }}>
+                    <Text style={{ fontSize: 16, fontWeight: 'bold', flexShrink: 1}} numberOfLines={1}>{item.getName()}</Text>
+                    <TouchableOpacity onPress={() => addClub(parseInt(item.getId()), item.getName(), item.getType())} style={{ marginLeft: 'auto', marginRight: 5 }}>
                           <MaterialIcons name="person-add" size={24} color="black" />
                         </TouchableOpacity>
                         <MaterialIcons name="arrow-forward-ios" size={20} color="black" />
@@ -112,9 +157,6 @@ export default function AddClubScreen({ navigation }) {
 
     const myRenderItem = ({ item }: { item: NestedOrganization }) => {
         const screenWidth = Dimensions.get('window').width;
-
-        // Find the corresponding full organization data from the filtered list
-        
 
         return (
             <Card style={{
@@ -133,9 +175,13 @@ export default function AddClubScreen({ navigation }) {
                 <TouchableOpacity onPress={() => navigation.navigate('misc/ClubDetails', {
                     id : item.getId(),
                     name: item.getName(),
+                    description: "fetch",
+                    day: "fetch",
+                    time: "fetch",
+                    location: "fetch"
                 })}>
-                    <View style={{ flexDirection: 'row', alignItems: 'center', flexWrap: 'wrap' }}>
-                        <Text style={{ fontSize: 16, fontWeight: 'bold', flexShrink: 1 }} numberOfLines={2}>{item.getName()}</Text>
+                    <View style={{ flexDirection: 'row', alignItems: 'center', flexWrap: 'nowrap' }}>
+                        <Text style={{ fontSize: 16, fontWeight: 'bold', flexShrink: 1}} numberOfLines={1}>{item.getName()}</Text>
                         <TouchableOpacity style={{ marginLeft: 'auto', marginRight: 5 }}>
                             <MaterialIcons name="person-remove" size={24} color="black" />
                         </TouchableOpacity>
--- app/misc/ClubDetails.tsx
+++ app/misc/ClubDetails.tsx
@@ -1,20 +1,92 @@
 import Divider from "@/components/Divider";
 import { formatDate } from "@/constants/FormatDate";
-import React from "react";
-import { ScrollView, View, Text } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useEffect, useState } from "react";
+import { ScrollView, View, Text, ActivityIndicator } from "react-native";
 
 export default function ClubDetails({ route, navigation }) {
-    const { name, description, day, time, location } = route.params;    
-    console.log(day, time, location);
+
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [day, setDay] = useState('');
+    const [time, setTime] = useState('');
+    const [location, setLocation] = useState('');
+    const [loading, setLoading] = useState(true);
+
+    function formatDayOfWeek(day: number) {
+        switch (day) {
+            case 0:
+                return 'Monday';
+            case 1:
+                return 'Tuesday';
+            case 2:
+                return 'Wednesday';
+            case 3:
+                return 'Thursday';
+            case 4:
+                return 'Friday';
+            case 5:
+                return 'Saturday';
+            case 6:
+                return 'Sunday';
+            default:
+                return '';
+        }
+    }
+
+
+    useEffect(() => {
+        const { id, name, description, day, time, location } = route.params;
+        const fetchData = async () => {
+            try {
+                let accessToken = await AsyncStorage.getItem('accessToken');
+                let response = await fetch(`https://fremont-app-backend.vercel.app/api/orgs/${id}`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        "Authorization": `Bearer ${accessToken}`, // Add this line for authorization
+                    },
+                });
+                let data = await response.json();
+                setName(data.name);
+                setDescription(data.description);
+                setDay(data.day);
+                setTime(data.time);
+                setLocation(data.location);
+            } catch (error) {
+                console.error('Error fetching organization:', error);
+            }
+        }
+        if (description == "fetch") {
+            fetchData().then(() => setLoading(false));
+        }
+        else {
+            setLoading(false);
+            setName(name);
+            setDescription(description);
+            setDay(day);
+            setTime(time);
+            setLocation(location);
+        }
+    });
+
+    if (loading) {
+        return (
+          <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <ActivityIndicator size='large' color='#BF1B1B'/>
+          </View>
+        )
+    }
+
     return (
         <ScrollView style={{ flex: 1, backgroundColor: '#F9F9F9', padding: 16 }}>
             <View style={{ marginBottom: 8 }}>
                 <Text style={{ fontSize: 18, fontWeight: 'bold', color: '#333' }}>{name}</Text>
                 {day == null && time == null && location == null ? null : 
                 location !== null ?
-                <Text style={{ fontSize: 14, color: '#666' }}>{day} {time} @ {location}</Text>
+                <Text style={{ fontSize: 14, color: '#666' }}>{formatDayOfWeek(Number(day))} {time} @ {location}</Text>
                 :
-                <Text style={{ fontSize: 14, color: '#666' }}>{day} {time}</Text>
+                <Text style={{ fontSize: 14, color: '#666' }}>{formatDayOfWeek(Number(day))} {time}</Text>
                 }
             </View>
             <Divider />
